e6c009ebb926ce0f7a0091750a87f16588a1950e
Patrik Bergman <38034479+ptrkbrgmn@users.noreply.github.com>
2022-12-15 18:14:37 +0100

    Added comment to middleware

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,9 @@ import validate from "./validators/validate";
 
 export async function middleware(req) {
   if (req.method == "POST") {
+    // Frågan är bara vad man ska göra här om man får valideringsfel?
+    // Man kan i princip bara göra redirect till en sida under /pages om jag förstår rätt.
+    // Man kan inte skriva till response.body numera, såvitt jag förstår.
     await validate(req);
   }
   return NextResponse.next();
